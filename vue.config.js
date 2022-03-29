ee48663a0996488cf49285c7393a92b44cdaf1c5
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-03-29 19:59:51 +0800

    chore: build config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  configureWebpack: {
+    externals: isProduction
+      ? {
+          wangeditor: "wangeditor",
+        }
+      : {},
+    output: {
+      libraryExport: "default",
+    },
+  },
 });
