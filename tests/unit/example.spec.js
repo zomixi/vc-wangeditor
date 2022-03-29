f116ce92462168136b2bb189a4aca4387b139343
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-03-29 19:57:36 +0800

    chore: add test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+import Component from "@/index.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("Component", () => {
+  it("is a Vue instance", () => {
+    const wrapper = mount(Component);
+    expect(wrapper.vm).toBeTruthy();
   });
 });
