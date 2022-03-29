b6f9235263b514f45a83f9a702357a67ecaa1340
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-03-29 19:49:44 +0800

    chore: config babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3,
+      },
+    ],
+  ],
+  plugins: ["@babel/plugin-transform-runtime"],
 };
