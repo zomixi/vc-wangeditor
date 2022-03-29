b4946fd570854409e6d86df47519ff478bc4a27f
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-03-29 19:58:22 +0800

    chore:  add demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Vue2WangEditor v-model="content" />
+
+    <textarea
+      style="width: 100%; height: 300px; margin-top: 24px"
+      v-model="content"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue2WangEditor from "./index.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Vue2WangEditor,
+  },
+  data() {
+    return {
+      content: "<p>hello world</p>",
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 64px;
 }
 </style>
