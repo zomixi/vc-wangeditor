f49ff302c258c7c5e9ab4be20aea44fabc16cbe1
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-03-30 15:21:10 +0800

    chore: remove externals

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 
-const isProduction = process.env.NODE_ENV === "production";
-
 module.exports = defineConfig({
+  transpileDependencies: true,
   configureWebpack: {
-    externals: isProduction
-      ? {
-          wangeditor: "wangeditor",
-        }
-      : {},
     output: {
       libraryExport: "default",
     },
