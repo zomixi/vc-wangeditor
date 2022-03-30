dd742a34f23d500987ca76a590043e64522e32ac
zomixi <zomixi@qq.com>
2022-03-30 22:39:34 +0800

    chore: fix build publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath: "/vue2-wangeditor", // just for github pages
   transpileDependencies: true,
   configureWebpack: {
     output: {
